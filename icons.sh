c70df537ccd7ebf15ea224a001373c3b7a9446ec
Miki Tebeka <miki@353solutions.com>
2021-02-10 16:41:32 +0200

    remove tickets button

---
+++ icons.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+convert -resize '57x57!' ~/Downloads/gcil-logo.png ~/Downloads/gcil-57x57.png
+convert -resize '200x200!' ~/Downloads/gcil-logo.png ~/Downloads/gcil-200x200.jpg
+convert -resize '215x215!' ~/Downloads/gcil-logo.png ~/Downloads/gcil-215x215.jpg
